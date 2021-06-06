c7be1e9987b70d9c4f29f9f934e356dacaca1238
all0cer <italoalmeinasc@gmail.com>
2021-06-06 20:02:10 -0300

    Change test

--- delivery/tests/test_app.py
+++ delivery/tests/test_app.py
@@ -6,4 +6,4 @@ def test_config_is_loaded(config):
     assert config["DEBUG"] is False
 
 def test_request_returns_404(client):
-    assert client.get("/").status_code == 404
+    assert client.get("/").status_code == 500
